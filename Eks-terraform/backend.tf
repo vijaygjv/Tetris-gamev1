620a91d5d2d2d8fcb3dfd5b29511559896029892
vijaygjv <vijayganesanofficial@gmail.com>
2024-09-02 22:52:30 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
+    bucket = "mybucketvijay12" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-2"
   }
 }
